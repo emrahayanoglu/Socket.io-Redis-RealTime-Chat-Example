3c3becb32d5922895698af866bf41570fcc6c5bb
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2011-10-07 12:22:49 +0300

    port updated on chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -7,7 +7,7 @@ io.configure(function () {
   io.set("polling duration", 10); 
 });
 
-app.listen(8000);
+app.listen(51100);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
