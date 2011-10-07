c6c5d438bfa3368b1e61e5a47f2651acf41dc6fe
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2011-10-07 12:11:40 +0300

    add some details to the files

---
+++ chatServer.js
@@ -0,0 +1,49 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+if (process.env.REDISTOGO_URL) {
+	// inside if statement
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+io.sockets.on('connection', function (client) {
+	
+	redis.subscribe("emrchat");
+	
+    redis.on("message", function(channel, message) {
+        client.send(message);
+    });
+
+    client.on('message', function(msg) {
+    });
+
+    client.on('disconnect', function() {
+        subsClient.quit();
+    });
+});
